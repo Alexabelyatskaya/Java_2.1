936735079b31e6c971505c43f377e41d7478d7bb
Alexandra <alexabelyatskaya@yandex.ru>
2021-10-02 09:05:52 +0300

    Task 2.1

---
+++ src/Task2_1.java
@@ -0,0 +1,10 @@
+public class Task2_1 {
+    public static void main(String[] args) {
+
+        int price = 5000;
+        int step = 20;
+        int bonus = price / step;
+
+        System.out.println(bonus);
+    }
+}
